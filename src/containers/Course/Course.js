ed80587d7f1603df3888233d7e1af7ed8bf8652c
RedLeader12 <redleader@jigsaw.xyz>
2018-05-29 18:22:20 +0800

    Passed in query string for title to course component

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+    state={
+        title: ''
+    }
+
+    componentDidMount(){
+        this.queryParamsHandler();
+    }
+
+    componentDidUpdate(){
+        this.queryParamsHandler();
+    }
+
+    queryParamsHandler(){
+        const query = new URLSearchParams(this.props.location.search)
+        for(let param of query.entries()){
+            console.log(query);
+            if(this.state.title !== param[1])
+            this.setState({title: param[1]})
+        }
+    }
+
+
     render () {
-        console.log(this.props)
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
+                <h1> {this.state.title}</h1>
                 <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
