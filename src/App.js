12aa88cd56bcdf1bf32fa87a31e9f8d5809449f7
RedLeader12 <redleader@jigsaw.xyz>
2018-05-29 17:53:42 +0800

    Prepared exercise for testing

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Courses from './containers/Courses/Courses';
+import Users from './containers/Users/Users';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <ol style={{textAlign: 'left'}}>
+         
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default App;
