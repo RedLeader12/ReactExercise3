b6c1d1fb4d01bfe6f662ddc8afd871afc60629b9
RedLeader12 <redleader@jigsaw.xyz>
2018-05-29 18:00:07 +0800

    Added Routes to load Users and Courses on different pages (by entering a URL, without Links)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom'
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -8,8 +9,12 @@ class App extends Component {
     return (
       <div className="App">
         <ol style={{textAlign: 'left'}}>
-         
+          <ul><NavLink to="/users">Users</NavLink> </ul> 
+          <ul><NavLink to="/courses">Courses</NavLink> </ul> 
+          <li> </li> 
         </ol>
+        <Route path="/users" exact component={Users}/> 
+        <Route path="/courses" component={Courses}/> 
       </div>
     );
   }
